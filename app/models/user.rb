da05bd38ec80894cf6b4d1a56115c3485c6529a7
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-02-12 16:06:33 +0900

    test code redo

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,25 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :password, length: { minimum: 6}
     validates :nickname
-    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kanji
+    validates :first_name_kanji
+    validates :last_name_kana 
+    validates :first_name_kana
     validates :birthday
+  end
+
+  with_options format: { with: /\A[a-zA-Z0-9]+\z/ } do
+    validates :password
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/ } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              { Faker::Name }
+    nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
+    last_name_kanji       {'山田'}
+    first_name_kanji      {'太郎'}
+    last_name_kana        {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birthday              {'20000101'}
   end
 end
