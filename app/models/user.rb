dcd4b15f83cfacf27f4617bbbad40c656606df84
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-02-12 09:05:49 +0900

    validation correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :password, length: { minimum: 6}
+    validates :nickname
+    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
+
 end
