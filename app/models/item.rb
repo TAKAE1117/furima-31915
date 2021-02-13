6d662ef77474a2dee5b6603d2d43de1d15aca38c
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-02-13 11:12:58 +0900

    validation settings

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,28 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :fee_id
+    validates :area_id
+    validates :day_id
+    validates :price 
+  end
+
+  with_options format: { with: greater_than_or_equal_to: 300 } do
+    validates :price
+  end
+
+  with_options format: { with: less_than_or_equal_to: 9999999 } do
+    validates :price
+  end
+
+  with_options format: { with: /\A[0-9]+\z/ } do
+    validates :price
+  end
 end
