196a3c77e7f417d275fb368b3c04247c62488745
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-02-23 17:53:47 +0900

    validation settings

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,17 @@
 class PurchaseAddredd
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, 
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code,   format: { with: /\A\d{3}[-]\d{4}\z/, message: "Postal code Input correctly" }
+    validates :prefecture_id
+    validates :municipality
+    validates :address
+    validates :phone_number
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+  
 end
