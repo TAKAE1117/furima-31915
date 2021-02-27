8f65987a20852d232dfd4988d40f8b718f68e27a
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-02-27 19:12:49 +0900

    routing settings for create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :purchases, only: :index
+    resources :purchases, only: [:index, :create]
   end
 end
