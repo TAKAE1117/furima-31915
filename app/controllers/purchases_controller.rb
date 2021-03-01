82d2dbff88d72bb21e21c7dca468ada7bb970e64
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-03-01 22:22:14 +0900

    implementation to save the value in DB

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,23 +1,22 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
   
   def create
-    # @purchase_address = PurchaseAddress.new(purchase_address_params)
-    # if @purchase_address.valid?
-    #   @purchase_address.save
-    #   redirect_to action: :index
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    @purchase_address.save
+    #   redirect_to root_path
     # else
-    #   render action: :new
+    #   render action: :create
     # end
   end
 
-  # private
+  private
 
-  # def purchase_address_params
-  #   params.require(:PurchaseAddress).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number)
-  # end
+  def purchase_address_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code,   format: { with: /\A\d{3}[-]\d{4}\z/, message: "Postal code Input correctly" }
@@ -15,6 +15,7 @@ class PurchaseAddress
   end
   
   def save
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
