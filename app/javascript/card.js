018538a28551253cfcb68dc94d736c02a98d9fed
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-03-06 15:36:24 +0900

    get browser card information

---
+++ app/javascript/card.js
@@ -0,0 +1,18 @@
+const pay = () => {
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+  });
+};
+
+window.addEventListener("load", pay);
